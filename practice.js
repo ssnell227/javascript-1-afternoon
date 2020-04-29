f6c48f27ab2f3137a1198d12c3765b9db5b0bc06
ssnell227 <ssnell227@gmail.com>
2020-04-29 12:22:55 -0600

    first commit

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+const myName = 'Stephen'
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+const myAge = 28
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+const lovesCode = true
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+const greatestFear = undefined
 
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
+const devMountainGoal = null
 
 //////////////////PROBLEM 6////////////////////
 
@@ -35,21 +35,25 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting (name) {
+  return `Hello, ${name}`
+}
 
 //////////////////PROBLEM 7////////////////////
 
 //Write a function expression called newGreeting.
 //Give it the same functionality as the function greeting in Problem 6.
 
-//Code Here
+const newGreeting = function (name) {
+  return `Hello, ${name}`
+}
 
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+const groceries = ['apples', 'milk', 'eggs', 'bread']
 
 //////////////////PROBLEM 9////////////////////
 
